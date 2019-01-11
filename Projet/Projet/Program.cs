e6a7966bf3338d5ca5bdff3cf727adec2cd66abc
Peleyah <peleyah@mail.com>
2019-01-11 03:02:44 +0000

    Evolution :airplane:

--- Projet/Projet/Program.cs
+++ Projet/Projet/Program.cs
@@ -6,20 +6,37 @@ using System.Threading.Tasks;
 
 namespace Projet
 {
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            int n = 0;
+
+            string[] vec = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vec[i] =String.Empty;
+
+            }
+
             int opc;
             do
             {
-                Console.WriteLine("[1] - Mensagem Boas Vindas");
-                Console.WriteLine("[2] - Primeiro e Último Nome");
-                Console.WriteLine("[3] ");
-                Console.WriteLine("[4] ");
-                Console.WriteLine("[5] ");
-                Console.WriteLine("[0] Sair do Programa");
-                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("<-------------------------FT01------------------------->");
+                Console.WriteLine("|-------------------------EXE1-------------------------|");
+                Console.WriteLine("| [1] - Mensagem Boas Vindas - [A. B. C.]              |");
+                Console.WriteLine("| [2] - Nome completo = Primeiro e Último Nome - [D.]  |");
+                Console.WriteLine("| [3] - Ler N do Vec das Strings - []                  |");
+                Console.WriteLine("| [4] -  Ler Vec das Strings - []                      |");
+                Console.WriteLine("| [5] - Maior string - [E.]                            |");
+                Console.WriteLine("| [6] - Strings mesmo tamanho? - [F.]                  |");
+                Console.WriteLine("| [7] - Soma Lenghts                                   |");
+                Console.WriteLine("| [0] Sair do Programa                                 |");
+                Console.WriteLine("<------------------------------------------------------>");
                 Console.Write("Digite uma opção: ");
                 opc = Int32.Parse(Console.ReadLine());
                 switch (opc)
@@ -31,13 +48,62 @@ namespace Projet
                         firstLastName();
                         break;
                     case 3:
-                        topString();
+                        n=lerN();
+                        break;
+                    case 4:
+                        vec=lerVec(n);
+                        break;
+                    case 5:
+                        longerString(n, vec);
+                        break;
+                    case 6:
+                        if (!sameSizeStrings(n, vec))
+                        {
+                            Console.WriteLine("As strings introduzidas não são iguais.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("As strings introduzidas são iguais.");
+                        }
+
+                        Console.WriteLine("Prima qualquer tecla para voltar ao menu...");
+                        break;
+                    case 7:
+                        if (somaLenghts(n,vec) > 0)
+                        {
+                            Console.Write("Soma das Lenghts: " + somaLenghts(n,vec) + ".");
+                        }
+                        Console.WriteLine("Prima qualquer tecla para voltar ao menu...");
                         break;
                 }
                 Console.ReadKey();
                 Console.Clear();
             }
             while (opc != 0);
+        }
+
+        public static int lerN()
+        {
+            Console.WriteLine("Quantas o valor de N? ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            return n;
+        }
+
+        public static string [] lerVec(int n)
+        {
+            string[] vec = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.WriteLine("String[ " + i + 1 + "]: ");
+                vec[i] = Console.ReadLine();
+
+            }
+
+            return vec;
+
         }
 
         private static void saiPrograma()
@@ -78,19 +144,68 @@ namespace Projet
 
         }
 
-        private static void topString()
+        private static void longerString(int n, string[] vec)
         {
             
+            string biggestString = "";
 
-            Console.WriteLine("Quantas strings deseja adicionar? ");
-            int n = Console.ReadLine();
+            for (int i = 0; i < n; i++)
+            {
 
 
-            Console.WriteLine("Prima qualquer tecla para voltar ao menu...");
+                int biggestDim = vec[0].Length;
+                
+
+                if (vec[i].Length > biggestDim)
+                    biggestString = vec[i];
+
+ 
+            }
+
+            Console.WriteLine("A string que tem o comprimento superior é " + biggestString + ".");
+            
+
+
 
 
         }
 
+        private static bool sameSizeStrings(int n, string []vec)
+        {
+
+            int contStrings = 0;
+
+            int lenghtVec0 = vec[0].Length;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                if(lenghtVec0 == vec[i].Length)
+                    contStrings++;
+
+            }
+
+            if (contStrings == n)
+                return true;
+            return false;
+
+        }
+
+        private static int somaLenghts(int n, string[] vec)
+        {
+
+            int somaLenghts = vec[0].Length;
+
+            for (int i = 1; i < n; i++)
+            {
+
+                somaLenghts += vec[i].Length;
+
+            }
+
+                return somaLenghts;
+
+        }
 
     }
 }
